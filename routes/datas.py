d47f73d59cf3d0f7e0eb0783c66103f70ac90f24
zxpilipala <eric@example.com>
2018-05-06 14:02:49 +0800

    add route to update

--- routes/datas.py
+++ routes/datas.py
@@ -1,10 +1,14 @@
 import json
+import requests
 
 from flask import Blueprint, request
 from models.Data import Gateway, Sensor, Datas
 
 
 main = Blueprint('datas', __name__)
+headers = {
+    "userkey": "6204472cfcc94a81b90e8d22773ca99e",
+}
 
 
 @main.route("/update-datas/<string:sensor_id>", methods=["POST"])
@@ -30,3 +34,58 @@ def update_equip(username):
             ss.gateway = gw
             ss.save()
     return "OK"
+
+
+@main.route("/get-all-update", methods=["GET"])
+def get_all_update():
+
+    json_str = get_all_gateway_json(headers)
+    gateway_list = json.loads(json_str)
+    for gateway in gateway_list:
+
+        # update equips
+        gw = Gateway(gateway)
+        gw.save()
+        for sensor in gateway["sensors"]:
+            ss = Sensor(sensor)
+            ss.gateway = gw
+            ss.save()
+
+        # update datas
+        for sensor in gateway["sensors"]:
+            sensor_id = sensor["id"]
+            url = "http://www.lewei50.com/api/v1/sensor/gethistorydata/{}".format(sensor_id)
+            r = requests.get(url, headers=headers)
+            data_list = json.loads(r)["Data"]
+            sensor = Sensor.query.filter_by(sensor_id=sensor_id).first()
+            for data_dict in data_list:
+                new_data = Datas(data_dict)
+                new_data.sensor = sensor
+                new_data.save()
+            return "OK"
+
+
+def get_all_gateway_json(headers):
+    url = "http://www.lewei50.com/api/v1/user/getsensorswithgateway"
+    r = requests.get(url, headers=headers)
+    json_str = r.content.decode("utf-8")
+    return json_str
+
+
+def update_datas():
+    json_str = get_all_gateway_json(headers)
+    gateway_list = json.loads(json_str)
+    for gateway in gateway_list:
+        for sensor in gateway["sensors"]:
+            sensor_id = sensor["id"]
+            url = "http://www.lewei50.com/api/v1/sensor/gethistorydata/{}".format(sensor_id)
+            r = requests.get(url, headers=headers)
+            json_str = r.content.decode("utf-8")
+            rsp = requests.post("http://0.0.0.0:3000/datas/update-datas/{}".format(sensor_id), json=json_str)
+            print(rsp)
+
+
+def update_gateway():
+    json_str = get_all_gateway_json(headers)
+    rsp = requests.post("http://0.0.0.0:3000/datas/update-equip/{}".format("poisonhz"), json=json_str)
+    print(rsp)
