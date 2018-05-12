2e78204afa3ac6807eeb383749ef707989a1646e
zxpilipala <eric@example.com>
2018-05-12 22:01:38 +0800

    ignore auto_get_datas

--- auto_get_datas.py
+++
@@ -1,9 +0,0 @@
-import requests
-import time
-
-
-if __name__ == "__main__":
-    # while True:
-        r = requests.get(url="http://localhost:3000/datas/update-equip/2")
-        r = requests.get(url="http://localhost:3000/datas/update-datas/2")
-# time.sleep(60)
